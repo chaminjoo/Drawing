d62a8f690ad33169f58ea61fe6ff506d6b162ab5
chaminjoo <k22tmfrl@naver.com>
2014-09-11 13:41:43 +0900

    add sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 function setup() {
   createCanvas(640, 480);
-  background(0);
+//background(red, green, blue);
+  background(255, 0, 0);
   stroke(255);
   noFill();
+
   line(10, 10, 90, 10);
     rect(20, 20, 80, 50);
   ellipse(50, 50, 50, 50);
